430ddd023c451ee3e528ac60ce68f0e095b212e1
grewer <grewer@grewer.cn>
2021-07-31 23:33:40 +0800

    feat: 具有给定数值的最小字符串2

--- 具有给定数值的最小字符串/index.js
+++ 具有给定数值的最小字符串/index.js
@@ -10,3 +10,45 @@
  */
 
 
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getSmallestString = function (n, k) {
+    const str = []
+    while (n > 0) {
+        // 比如 k = 27 n=3  1 >= 2 这种情况无法通过
+        if (k - 26 >= n - 1) {
+            n--
+            str[n] = 'z'
+            k -= 26
+        } else if (k > n) {
+            // 比如 k = 24 n=2  小于 26   k-n-1+97  97=a
+            // a-z 中间的情况
+            n--
+            str[n] = String.fromCodePoint(k - n + 96)
+            k = n - 1
+        } else {
+            n--
+            str[n] = 'a'
+            k--
+        }
+    }
+    return str.join('')
+};
+
+// 这个算法的结果是正确的  但是效率偏慢:
+// 执行用时：
+// 356 ms
+// , 在所有 JavaScript 提交中击败了
+// 12.00%
+// 的用户
+// 内存消耗：
+// 59.5 MB
+// , 在所有 JavaScript 提交中击败了
+// 6.00%
+// 的用户
+
+
+// 相同的算法 在 Java 中 可以击败 90%的用户
